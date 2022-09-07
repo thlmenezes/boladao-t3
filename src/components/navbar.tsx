65acd6d37583e14a112e1e9ff0e3371641f106d3
Thales Menezes <thlmenezes@pm.me>
2022-09-07 19:51:09 -0300

    ✨ navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,86 @@
+import { signIn, signOut } from "next-auth/react"
+import { trpc } from "../utils/trpc";
+import Link from 'next/link'
+
+export function Navbar(){
+  const session = trpc.auth.getSession.useQuery();
+  const user = session.data?.user;
+
+  function sessionButton(){
+    if(!user)
+      return <button onClick={() => signIn("twitch")}>Login</button>
+    return <button onClick={() => signOut()}>Log out</button>
+  }
+
+  function userAvatar(){
+    if(!user) return <p>Avatar</p>
+
+    return <picture>
+      <img
+        className="rounded-full"
+        src={user.image ?? ''}
+        alt={`Foto de Perfil do usuário ${user.name}`}
+        width={50}
+        height={50}
+        />
+    </picture>
+  }
+
+  return (
+    <nav className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown md:hidden">
+          <label tabIndex={0} className="btn btn-ghost btn-circle">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+          </label>
+          <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+            <Link href="/">
+              <li><button>Início</button></li>
+            </Link>
+            {
+              user &&
+              <>
+                <Link href="/posts">
+                  <li><button>Posts</button></li>
+                </Link>
+                <Link href="/posts/new">
+                  <li><button>Novo Post</button></li>
+                </Link>
+              </>
+            }
+          </ul>
+        </div>
+      </div>
+      <div className="navbar-end">
+        <div className="space-x-5 mr-5 invisible md:visible">
+          <Link href="/">
+            <button className="btn btn-ghost">Início</button>
+          </Link>
+          {
+            user &&
+            <>
+              <Link href="/posts">
+                <button className="btn btn-ghost">Posts</button>
+              </Link>
+              <Link href="/posts/new">
+                <button className="btn btn-ghost">Novo Post</button>
+              </Link>
+            </>
+          }
+        </div>
+        <div className="dropdown dropdown-end">
+        <div className="tooltip tooltip-left" data-tip={user?.name ?? ""}>
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10">
+              {userAvatar()}
+            </div>
+          </label>
+        </div>
+        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+          <li>{sessionButton()}</li>
+        </ul>
+      </div>
+      </div>
+    </nav>
+  );
+}
