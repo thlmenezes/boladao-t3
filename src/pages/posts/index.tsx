d6b0fcbbb195ff2170ee0e5678f96f7bd7c473e9
Thales Menezes <thlmenezes@pm.me>
2022-09-07 21:15:31 -0300

    💄 use grid of cards for posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -8,6 +8,11 @@ const Posts: NextPage = () => {
   const postsData = trpc.post.getAllPosts.useQuery();
   const { data } = useSession();
 
+  const copyTextToClipboard = (text: string) => () => {
+    if (typeof window === "undefined") return;
+    navigator.clipboard.writeText(text);
+  };
+
   function formatPostsData(posts: typeof postsData.data) {
     if (!posts || posts.length === 0)
       return <a className="mt-3 text-sm underline text-violet-500 decoration-dotted underline-offset-2"
@@ -16,7 +21,14 @@ const Posts: NextPage = () => {
             Criar Novo Post
           </a>
     return posts.map((post) => {
-      return <p key={post.id}>{post.description}</p>
+      return <div key={post.id} className="card card-bordered w-96 bg-neutral text-neutral-content shadow-xl">
+      <div className="card-body">
+        <p>{post.description}</p>
+        <div className="card-actions justify-end">
+          <button onClick={copyTextToClipboard(post.description)} className="btn btn-primary">Copiar</button>
+        </div>
+      </div>
+    </div>
     })
   }
   return <div className="flex flex-col min-h-screen">
@@ -29,11 +41,11 @@ const Posts: NextPage = () => {
       <main className="container flex flex-1 flex-col items-center justify-center p-4 mx-auto">
         {!data ?
           <h1>Deslogado</h1>
-          : <>
+          : <div className="grid gap-5 md:grid-cols-2 2xl:grid-cols-3">
             {
               formatPostsData(postsData.data)
             }
-          </>
+          </div>
         }
       </main>
   </div>
