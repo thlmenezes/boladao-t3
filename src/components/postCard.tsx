30d6c59a040d6cfc9d903bfe8f5068149f9723bd
Thales Menezes <thlmenezes@pm.me>
2022-09-27 02:30:12 -0300

    ✨ edit and delete buttons to post card

--- src/components/postCard.tsx
+++ src/components/postCard.tsx
@@ -36,6 +36,32 @@ export function PostCard({
         )}
         <p>{data.description}</p>
         <div className="card-actions justify-end">
+          <button className="btn btn-square">
+            <picture>
+              <img
+                width={25}
+                height={25}
+                src="/pencil.svg"
+                alt={'clique para editar'}
+              />
+            </picture>
+          </button>
+          <button className="btn btn-square hover:bg-red-800">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
           <button
             onClick={copyTextToClipboard(
               data.description + '\nCopiado de projeto boladão <3'
